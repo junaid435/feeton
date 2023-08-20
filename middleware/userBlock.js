abd05750378fe221a8a9ea1e28b8a45fab21ec27
junaid <junaidvinu133@gmail.com>
2023-08-20 17:45:15 +0530

    console change

--- middleware/userBlock.js
+++ middleware/userBlock.js
@@ -3,7 +3,7 @@ const user=require('../models/User_Model')
  const userblockCheck=async(req,res,next)=>{
     if(req.session.userid!==undefined){
         const userData=await user.findById({_id:req.session.userid})
-        console.log(userData);
+       
         if (userData.is_block===true) {
             req.session.destroy()
             res.redirect('/login')
